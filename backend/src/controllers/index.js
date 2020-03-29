5eb243a6bf4b15153ce7a998b9797604bd8cc12e
Rafael Silveira <sepolrafael@gmail.com>
2020-03-28 21:46:41 -0300

    Created backend files from application

---
+++ backend/src/controllers/index.js
@@ -0,0 +1,11 @@
+const IncidentController = require('./IncidentController');
+const OngController = require('./OngController');
+const ProfileController = require('./ProfileController');
+const SessionController = require('./SessionController');
+
+module.exports = {
+    IncidentController,
+    OngController,
+    ProfileController,
+    SessionController
+}
